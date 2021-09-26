d5684459e0218bc3e33d0e9d192f0b634492a84b
Skyslycer <skyslycer@gmx.net>
2021-09-26 19:16:55 +0200

    add repo pushing functionality

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     java
+    `maven-publish`
 }
 
 group = "de.skyslycer"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
@@ -15,6 +16,72 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
 }
 
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+publishing {
+    val publishData = PublishData(project)
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+        groupId = project.group as String?
+        artifactId = project.name
+        version = publishData.getVersion()
+    }
+
+    repositories {
+        maven {
+            authentication {
+                credentials(PasswordCredentials::class) {
+                    username = System.getenv("NEXUS_USERNAME")
+                    password = System.getenv("NEXUS_PASSWORD")
+                }
+            }
+
+            name = "SkyNexus"
+            url = uri(publishData.getRepository())
+        }
+    }
+}
+
+class PublishData(private val project: Project) {
+    var type: Type = getReleaseType()
+    var hashLength: Int = 7
+
+    private fun getReleaseType(): Type {
+        val branch = getCheckedOutBranch()
+        return when {
+            branch.contentEquals("master") -> Type.RELEASE
+            branch.startsWith("dev") -> Type.DEV
+            else -> Type.SNAPSHOT
+        }
+    }
+
+    private fun getCheckedOutGitCommitHash(): String = System.getenv("GITHUB_SHA")?.substring(0, hashLength) ?: "local"
+
+    private fun getCheckedOutBranch(): String = System.getenv("GITHUB_REF")?.replace("refs/heads/", "") ?: "local"
+
+    fun getVersion(): String = getVersion(false)
+
+    fun getVersion(appendCommit: Boolean): String =
+        type.append(getVersionString(), appendCommit, getCheckedOutGitCommitHash())
+
+    private fun getVersionString(): String = (project.version as String).replace("-SNAPSHOT", "").replace("-DEV", "")
+
+    fun getRepository(): String = type.repo
+
+    enum class Type(private val append: String, val repo: String, private val addCommit: Boolean) {
+        RELEASE("", "https://repo.skyslycer.de/repository/maven-releases/", false),
+        DEV("-DEV", "https://repo.skyslycer.de/repository/maven-dev/", true),
+        SNAPSHOT("-SNAPSHOT", "https://repo.skyslycer.de/repository/maven-snapshots/", true);
+
+        fun append(name: String, appendCommit: Boolean, commitHash: String): String =
+            name.plus(append).plus(if (appendCommit && addCommit) "-".plus(commitHash) else "")
+    }
 }
